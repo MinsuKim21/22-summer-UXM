f038757d24a342fd2fed6564e29ca7f9e93df855
MinsuKim <minsu020302@gmail.com>
2022-07-22 22:36:58 +0900

    Feat : add Boardside component

--- src/Pages/BoardPage/BoardAdd.js
+++ src/Pages/BoardPage/BoardAdd.js
@@ -1,8 +1,15 @@
-import { Container, } from '@mui/material';
-// import BoardSide from '../../components/BoardSide';
+import { Container, Box} from '@mui/material';
+import BoardSide from '../../components/BoardSide';
 const New = () => {
   return (
-    <Container sx={{ display: 'flex' }}>이곳은 보드 작성페이지.</Container>
+    <Container sx={{ height: '100vh', mt: 2, display: 'flex' }}>
+      <Box width={'10%'}>
+        <BoardSide />
+      </Box>
+      <Box width={'90%'}>
+        fadsfasd
+      </Box>
+    </Container>
   );
 };
 export default New;
